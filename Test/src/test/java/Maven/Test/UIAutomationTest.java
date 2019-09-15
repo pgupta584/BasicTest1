f812138b1ba5407054e5b5b4ffb3e410196272b5
pankajgupta <pgupta584@gmail.com>
2019-09-15 18:20:32 +0530

    Commit

---
+++ Test/src/test/java/Maven/Test/UIAutomationTest.java
@@ -0,0 +1,16 @@
+package Maven.Test;
+
+import org.testng.annotations.Test;
+
+public class UIAutomationTest {
+	@Test
+	public void login()
+	{
+		System.out.println("Valid Login");
+	}
+	@Test
+	public void inValidlogin()
+	{
+		System.out.println("InValid Login");
+	}
+}
